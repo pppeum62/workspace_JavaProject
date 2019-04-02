daf6d7df51786cc30e777d699eb6a608f8979429
pppeum <s2018s13@e-mirim.hs.kr>
2019-04-02 22:14:04 +0900

    add StudentNumberToMajor

---
+++ 20190402_Major/src/StudentNumberToMajor.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class StudentNumberToMajor {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String major[][] = {{"뉴미디어소프트웨어과", "뉴미디어소프트웨어과",
+				"뉴미디어웹솔루션과", "뉴미디어웹솔루션과", "뉴미디어디자인과", "뉴미디어디자인과"},
+				{"뉴미디어소프트웨어과", "뉴미디어소프트웨어과",
+				"뉴미디어웹솔루션과", "뉴미디어웹솔루션과", "뉴미디어디자인과", "뉴미디어디자인과"},
+				{"인터랙티브미디어과", "인터랙티브미디어과","뉴미디어디자인과","뉴미디어디자인과",
+				"뉴미디어솔루션과","뉴미디어솔루션과"}};
+		
+		System.out.print("학번을 입력해주세요 >> ");
+		String input = sc.nextLine();
+		
+		String grade = input.substring(0, 1);
+		String classNum = input.substring(1, 2);
+		
+		int gradeNum = Integer.parseInt(grade);
+		int classNumber = Integer.parseInt(classNum);
+		
+		System.out.println("학번 : "+input);
+		System.out.println("학과 : "+major[gradeNum-1][classNumber-1]);
+	}
+}
