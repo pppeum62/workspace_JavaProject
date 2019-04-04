d20f671fff0ace6f605cf30ec225c5577f0172c8
pppeum <s2018s13@e-mirim.hs.kr>
2019-04-04 22:33:01 +0900

    add 3-6-9 Game

---
+++ 20190404_3-6-9-Game/src/Game_369.java
@@ -0,0 +1,24 @@
+
+public class Game_369 {
+	public static void main(String[] args) {
+		for(int i = 1; i < 100; i++) {
+			int cnt=0;
+			if(i < 10)
+				if(i%3==0)
+					System.out.println("짝");
+				else
+					System.out.println(i);
+			else if(i/10%3==0) {
+				System.out.print("짝");
+				if(i%10!=0 && i%10%3==0)
+					System.out.println("짝");
+				else
+					System.out.println();
+			}
+			else if(i%10!=0 && i%10%3==0)
+				System.out.println("짝");
+			else
+				System.out.println(i);
+		}
+	}
+}
