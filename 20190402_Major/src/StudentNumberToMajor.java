941a23afb0b9ed92a5287abb8421191d7a4d5b67
pppeum <s2018s13@e-mirim.hs.kr>
2019-04-04 19:06:51 +0900

    StudentNumberToMajor change

--- 20190402_Major/src/StudentNumberToMajor.java
+++ 20190402_Major/src/StudentNumberToMajor.java
@@ -4,12 +4,9 @@ public class StudentNumberToMajor {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String major[][] = {{"뉴미디어소프트웨어과", "뉴미디어소프트웨어과",
-				"뉴미디어웹솔루션과", "뉴미디어웹솔루션과", "뉴미디어디자인과", "뉴미디어디자인과"},
-				{"뉴미디어소프트웨어과", "뉴미디어소프트웨어과",
-				"뉴미디어웹솔루션과", "뉴미디어웹솔루션과", "뉴미디어디자인과", "뉴미디어디자인과"},
-				{"인터랙티브미디어과", "인터랙티브미디어과","뉴미디어디자인과","뉴미디어디자인과",
-				"뉴미디어솔루션과","뉴미디어솔루션과"}};
+		String major[][] = {{"뉴미디어소프트웨어과","뉴미디어웹솔루션과", "뉴미디어디자인과"},
+				{"뉴미디어소프트웨어과","뉴미디어웹솔루션과", "뉴미디어디자인과"},
+				{"인터랙티브미디어과", "뉴미디어디자인과", "뉴미디어솔루션과"}};
 		
 		System.out.print("학번을 입력해주세요 >> ");
 		String input = sc.nextLine();
@@ -20,7 +17,9 @@ public class StudentNumberToMajor {
 		int gradeNum = Integer.parseInt(grade);
 		int classNumber = Integer.parseInt(classNum);
 		
+		int classNumber2 = (classNumber-1)/2;
+		
 		System.out.println("학번 : "+input);
-		System.out.println("학과 : "+major[gradeNum-1][classNumber-1]);
+		System.out.println("학과 : "+major[gradeNum-1][classNumber2]);
 	}
 }
