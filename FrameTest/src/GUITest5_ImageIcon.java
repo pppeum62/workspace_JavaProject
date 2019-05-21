3681a23161828b4f6dba2d153b08a40f31fcb20a
pppeum <s2018s13@e-mirim.hs.kr>
2019-05-21 19:37:36 +0900

    add setRolloverIcon, setPressedIcon

--- FrameTest/src/GUITest5_ImageIcon.java
+++ FrameTest/src/GUITest5_ImageIcon.java
@@ -10,12 +10,16 @@ public class GUITest5_ImageIcon {
 		ImageIcon Charlie = new ImageIcon("src/images/Charlie.png");
 		JLabel imageLabel = new JLabel(Charlie);
 		
+		ImageIcon kirby = new ImageIcon("src/images/kirby.png");
+		JLabel imageLb = new JLabel(kirby);
+		
 		ImageIcon Charlie2 = new ImageIcon("src/images/CharlieBr.png");
 		//JButton imageButton = new JButton(Charlie2);
 		//ImageIcon 크기 수정
 		ImageIcon smallCharlie = new ImageIcon(Charlie2.getImage().getScaledInstance(320, 600, Image.SCALE_DEFAULT));
-		
 		JButton imageButton = new JButton(smallCharlie);
+		imageButton.setRolloverIcon(kirby); //마우스 올렸을 때
+		imageButton.setPressedIcon(Charlie); //마우스 클릭하면
 		
 		panel.add(imageLabel);
 		panel.add(imageButton);
